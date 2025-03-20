7a1918b7daeedf527cca6baa2253ae08d520c370
sierra-uga <sb29930@uga.edu>
2025-03-20 11:36:47 -0400

    update

--- 1_import.sh
+++ 1_import.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #SBATCH --job-name=“import-data”
-#SBATCH --partition=campus
+#SBATCH --partition=campus-bigmem
 #SBATCH --nodes=1
-#SBATCH --qos=campus
+#SBATCH --qos=campus-bigmem
 #SBATCH -A ISAAC-UTK0268
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=15G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --time=1-00:00:00
 #SBATCH --mail-user=sbartle7@utk.edu
 #SBATCH --mail-type=END,FAIL
@@ -14,22 +14,16 @@
 #SBATCH -o errors/import-data.out-%N
 
 # import data
-module load gcc/10.2.0
-module load anaconda3/2021.05
-module load qiime2/2022.8
-input=/nfs/home/sbartle7/CERES/SynCom1/data-raw/fastq
-output=/nfs/home/sbartle7/CERES/SynCom1/data-clean/syncom1-demultiplexed-samples.qza
+module load gcc/10.2.0 #prereq for qiime
+module load anaconda3/2021.05 #prereq for qiime
+module load qiime2/2022.8 
+input=/lustre/isaac24/scratch/sbartle7/CERES/SynCom1/data-raw/UTK0268_jdebruyn_DeBruyn_Taylor_240924/syncom1-manifest-file.tsv
+output=/lustre/isaac24/scratch/sbartle7/CERES/SynCom1/data-clean/syncom1-demultiplexed-samples.qza
 
 # for demultiplexed sequences only!
 qiime tools import \
   --type 'SampleData[PairedEndSequencesWithQuality]' \
   --input-path $input \
-  --input-format CasavaOneEightSingleLanePerSampleDirFmt \
-  --output-path $output
+  --output-path $output \
+  --input-format PairedEndFastqManifestPhred33V2
   
-# visualize qza to see quality, and to know where to trim with dada2
-qiime demux summarize \
---i-data $output \
---o-visualization syncom1-demux.qza
-
-# visualize using --> www.view.qiime2.org 
