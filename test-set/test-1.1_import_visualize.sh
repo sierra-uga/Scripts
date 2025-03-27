ea1074b0fbc06a1abc237d3caf9dc5f04dd0d4f9
sierra-uga <sb29930@uga.edu>
2025-03-27 12:53:05 -0400

    im going insane

---
+++ test-set/test-1.1_import_visualize.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#SBATCH --job-name=“import-visual”
+#SBATCH --partition=campus-bigmem
+#SBATCH --nodes=1
+#SBATCH --qos=campus-bigmem
+#SBATCH -A ISAAC-UTK0268
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=1-00:00:00
+#SBATCH --mail-user=sbartle7@utk.edu
+#SBATCH --mail-type=END,FAIL
+#SBATCH -e /lustre/isaac24/scratch/sbartle7/CERES/SynCom1/scripts/errors/import-data.err-%N
+#SBATCH -o /lustre/isaac24/scratch/sbartle7/CERES/SynCom1/scripts/errors/import-data.out-%N
+
+# import data
+module load gcc/10.2.0
+module load anaconda3/2021.05
+module load qiime2/2022.8
+input=/lustre/isaac24/scratch/sbartle7/CERES/SynCom1/data-clean/test-syncom1-demultiplexed-samples.qza
+output=/lustre/isaac24/scratch/sbartle7/CERES/SynCom1/data-clean
+
+# visualize qza to see quality, and to know where to trim with dada2
+qiime demux summarize \
+--i-data $input \
+--o-visualization $output/test-syncom1-demux.qzv
+
+# visualize using --> www.view.qiime2.org 
---
+++ test-set/test-2_dada2.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#SBATCH --job-name=“dada2-analysis”
+#SBATCH --partition=long-bigmem
+#SBATCH --nodes=1
+#SBATCH --qos=long-bigmem
+#SBATCH -A ISAAC-UTK0268
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem-per-cpu=30G
+#SBATCH --time=6-00:00:00
+#SBATCH --mail-user=sbartle7@utk.edu
+#SBATCH --mail-type=END,FAIL
+#SBATCH -e /lustre/isaac24/scratch/sbartle7/CERES/SynCom1/scripts/errors/dada2.err-%N
+#SBATCH -o /lustre/isaac24/scratch/sbartle7/CERES/SynCom1/scripts/errors/dada2.out-%N
+
+# load/variables
+module load gcc/10.2.0 #prereq for qiime
+module load anaconda3/2021.05 #prereq for qiime
+module load qiime2/2022.8 
+input=/lustre/isaac24/scratch/sbartle7/CERES/SynCom1/data-clean
+output=/lustre/isaac24/scratch/sbartle7/CERES/SynCom1/data-clean
+
+# basic -- trims to 120 basepairs
+qiime dada2 denoise-paired \
+        --i-demultiplexed-seqs $input/test-syncom1-demultiplexed-samples.qza \
+        --p-n-threads 0 \
+        --p-trunc-len-f 240 \
+        --p-trunc-len-r 200 \
+        --p-chimera-method pooled \
+        --verbose \
+        --o-table $output/test-table-dada2.qza \
+        --o-representative-sequences $output/test-rep-seqs.qza \
+        --o-denoising-stats $output/test-stats-dada2.qza
